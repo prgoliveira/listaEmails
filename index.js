46e65f65a4990ee43fba87cdd4ba58a37018fad7
prgoliveira <prgoliveira@users.noreply.github.com>
2020-08-20 22:17:58 -0300

    Conceitos estudados de Promise e async/await -rf

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const fs = require('fs')
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
-const axios = require('axios')
-const onlyEmail = (pessoa) => pessoa.email
-//const onlyEmailRegex = new RegExp(/\S+@\w+\.\w{2,6}(\.\w{2})?/g)
- 
-axios.get(url).then(response =>{
-    const grupo = response.data
-    const emails = grupo.map(onlyEmail)
-    //console.log(grupo.match(/\S+\@\w+\.\w{2,6}(\.\w{2})?/g))
-    //console.log(JSON.stringify(grupo.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/g)))
-    fs.writeFile(__dirname + '/informacoes.json', JSON.stringify(grupo), err => {})
-    fs.writeFile(__dirname + '/emails.json', JSON.stringify(emails), err => {})
-})
+const http = require('http')
 
+const getInformation = endereco =>{
+    return new Promise((resolve, reject) =>{
+        http.get(endereco, res =>{
+            let information = ''
+            res.on('data', dados =>{
+                information += dados
+            })
 
-const arquivo = __dirname + '/emails.json'
-fs.readFile(arquivo, 'utf-8', (err, conteudo) =>{
-    const informacao = JSON.parse(conteudo)
-    console.log(informacao)
-})
+            res.on('end', () =>{
+                try{
+                    resolve(JSON.parse(information))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+let lista = async () => {
+    const funcionarios = await getInformation(url)
+    return funcionarios
+}
+
+lista()
+    .then(funcionario => funcionario.map(f => f.email))
+    .then(emails => fs.writeFile(__dirname + '/emails.json', JSON.stringify(emails), err => {}))
